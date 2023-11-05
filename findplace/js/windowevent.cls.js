f0bb27bef310bb59b2120a3fd2413f0ed0ca3a52
vairydler <vairydler@users.noreply.github.com>
2023-11-05 06:18:18 +0000

    urlchange

--- findplace/js/windowevent.cls.js
+++ findplace/js/windowevent.cls.js
@@ -4,7 +4,8 @@ class MyClass {
   }
 
   load() {
-    document.getElementById("gas").src = "https://script.google.com/macros/s/AKfycbx9KZxCEW-377BhTtqFjYgeHXzYEApE4Wrrwu-x51aYSbH9vIBLAVHxNSwLtDDkUNxv/exec" + new URL(window.location.href).search
+    /* 上位のURLパラメータを渡す */
+    document.getElementById("gas").src = "https://script.google.com/macros/s/AKfycbyqlztJJ5UC4RB0VCelvtZOZpRhqu9vyN85l_P71_kPpGMuVMHi00pBiJOeyzLArOWf/exec" + new URL(window.location.href).search
   }
 }
 
