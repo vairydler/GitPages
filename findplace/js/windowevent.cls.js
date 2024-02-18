7f4e29d811932a1c1489592ccc46b750e9f8ca1f
vairydler <vairydler@users.noreply.github.com>
2024-02-18 17:15:29 +0000

    GASのURL変更

--- findplace/js/windowevent.cls.js
+++ findplace/js/windowevent.cls.js
@@ -5,7 +5,7 @@ class MyClass {
 
   load() {
     /* 上位のURLパラメータを渡す */
-    document.getElementById("gas").src = "https://script.google.com/macros/s/AKfycbyqlztJJ5UC4RB0VCelvtZOZpRhqu9vyN85l_P71_kPpGMuVMHi00pBiJOeyzLArOWf/exec" + new URL(window.location.href).search
+    document.getElementById("gas").src = "https://script.google.com/macros/s/AKfycbwL-ag3y2y7UeAfmRwmN_JG-rU7RylLzr656y-RPiasfpQN9T2h0y4iCZSAH6If21Ru/exec" + new URL(window.location.href).search
   }
 }
 
