96620ec249567b9d6e366b72a1d3b9b0e4f921db
vairydler <vairydler@users.noreply.github.com>
2024-02-19 02:18:04 +0000

    不要なログ出力を削除

--- findplace/js/startup.js
+++ findplace/js/startup.js
@@ -6,7 +6,6 @@
     /* classのメソッドを取得するにはコレ */
     Object.getOwnPropertyNames(event.__proto__).forEach((e, i, a) => {
       window.addEventListener(e, event[e]);
-      console.log(event[e]);
     });
   });
 })();
