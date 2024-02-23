cdf4bef234b891d73f33105d960e932978fa96ca
vairydler <vairydler@users.noreply.github.com>
2024-02-23 08:45:45 +0000

    GASのURL変更

--- findplace/js/define.js
+++ findplace/js/define.js
@@ -1,1 +1,1 @@
-const gas = "https://script.google.com/macros/s/AKfycbxLjkjxEbCrY7AtG_vYH46Le5i7Ud6NwIuPhJfOQx738MzIXSz53xathSVta7c2RxQG/exec";
+const gas = "https://script.google.com/macros/s/AKfycbz7kXjqpNVsXGxZnmfCja72icN_4lBdAwOpDNoOx3WnJQMmoi8KH1O5m7QEp_RELrDY/exec";
